26c52cce826b04c403666020b448b38768bd1462
AlinaNadezhdina <nadezdinaalina99@gmail.com>
2023-02-17 16:59:55 +0300

    edit 21_stack.h style

--- src/s21_graph/s21_containers/s21_stack.h
+++ src/s21_graph/s21_containers/s21_stack.h
@@ -1,41 +1,40 @@
 #ifndef S21_STACK_H
-# define S21_STACK_H
+#define S21_STACK_H
 
-# include <strings.h>
+#include <strings.h>
 
-# include "s21_utils.h"
+#include "s21_utils.h"
 
-namespace s21
-{
-	template <class T>
-	class Stack
-	{
-	public:
-		Stack();
-		Stack&	operator=(const Stack &);
-		Stack(const Stack &);
-		Stack&	operator=(Stack &&);
-		Stack(Stack &&);							//конструктор перемещения
-		Stack(std::initializer_list<T> const& init);//конструктор со списком инициализации
-		~Stack();
-		void			init();// - создание пустого стек/очереди
-		void			push(T const &);// - добавление элемента в стек/очередь
-		T				&pop();// - получение элемента из стека/очереди с его последующим удалением из стека/очереди
-		T				&peek();// - получение элемента из стека/очереди без его удаления из стека/очереди
-		unsigned int	getSize();
-	private:
-		void	Copy(Stack &);
-		void	Clear();
-		struct StackItem
-		{
-			T			value;
-			StackItem	*next;
-		};
-		unsigned int	size_;
-		StackItem		*head_;
-	};
-}
+namespace s21 {
+template <class T>
 
-# include "s21_stack.tpp"
+class Stack {
+ public:
+  Stack();
+  Stack &operator=(const Stack &);
+  Stack(const Stack &);
+  Stack &operator=(Stack &&);
+  Stack(Stack &&);
+  Stack(std::initializer_list<T> const &init);
+  ~Stack();
+  void init();
+  void push(T const &);
+  T &pop();
+  T &peek();
+  unsigned int getSize();
+
+ private:
+  void Copy(Stack &);
+  void Clear();
+  struct StackItem {
+    T value;
+    StackItem *next;
+  };
+  unsigned int size_;
+  StackItem *head_;
+};
+}  // namespace s21
+
+#include "s21_stack.tpp"
 
 #endif
