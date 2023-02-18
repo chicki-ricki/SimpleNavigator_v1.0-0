af37005fbf978248525ed5b842a7f581d86dfc5e
Ev-genia <bzhenya2006@ya.ru>
2023-02-18 15:00:58 +0300

    s21_utils.h with google style

--- src/s21_graph/s21_containers/s21_utils.h
+++ src/s21_graph/s21_containers/s21_utils.h
@@ -1,15 +1,13 @@
 #ifndef S21_UTILS_H
-# define S21_UTILS_H
+#define S21_UTILS_H
 
-# include <iostream>
+#include <iostream>
 
-namespace s21
-{
-		void	exitError(std::string str)
-	{
-		perror(str.c_str());
-		exit(1);
-	}
+namespace s21 {
+void exitError(std::string str) {
+  perror(str.c_str());
+  exit(1);
 }
+}  // namespace s21
 
 #endif
