4cc8dff9238c3d66f68eabf17cb1242f55a44e56
AlinaNadezhdina <nadezdinaalina99@gmail.com>
2023-03-21 13:40:22 +0300

    tests with Dextra

--- src/s21_graph_algorithms_test.cpp
+++ src/s21_graph_algorithms_test.cpp
@@ -3,15 +3,51 @@
 #include "s21_graph_algorithms.h"
 
 TEST(depthFirstSearch_test, depthFirstSearch_data) {
-  int rez[11] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+  int rez[5] {1, 2, 4, 3, 5};
   Graph graph;
   GraphAlgorithms graphAlg;
   int start = 1;
-  int *exp = new int[11];
+  int *exp = new int[5];
 
-  graph.loadGraphFromFile("/Users/eugenia/IT_projects/06_sber21/A2_SimpleNavigator_v1.0-2/src/graph_11.txt");
+  graph.loadGraphFromFile("graph_5.txt");
   exp = graphAlg.depthFirstSearch(graph, start);
-  EXPECT_EQ(rez[10], exp[10]);
+  EXPECT_EQ(rez[2], exp[2]);
+}
+
+TEST(breadthFirstSearch_test, breadthFirstSearch_data) {
+  int rez[5] {1, 2, 3, 4, 5};
+  Graph graph;
+  GraphAlgorithms graphAlg;
+  int start = 1;
+  int *exp = new int[5];
+
+  graph.loadGraphFromFile("graph_5.txt");
+  exp = graphAlg.breadthFirstSearch(graph, start);
+  EXPECT_EQ(rez[3], exp[3]);
+}
+
+TEST(getShortestPathBetweenVertices_test, getShortestPathBetweenVertices_data1) {
+  int rez = 20;
+  Graph graph;
+  GraphAlgorithms graphAlg;
+  int start = 2;
+  int finish = 5;
+
+  graph.loadGraphFromFile("graph_5.txt");
+  int exp = graphAlg.getShortestPathBetweenVertices(graph, start, finish);
+  EXPECT_EQ(rez, exp);
+}
+
+TEST(getShortestPathBetweenVertices_test, getShortestPathBetweenVertices_data2) {
+  int rez = 130;
+  Graph graph;
+  GraphAlgorithms graphAlg;
+  int start = 2;
+  int finish = 5;
+
+  graph.loadGraphFromFile("graph_5_or.txt");
+  int exp = graphAlg.getShortestPathBetweenVertices(graph, start, finish);
+  EXPECT_EQ(rez, exp);
 }
 
 int main(int argc, char **argv) {
